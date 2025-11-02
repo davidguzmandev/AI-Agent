d7b484028b41d86b529996d4607881db28193791
davidruncode <daguz.91@gmail.com>
2025-11-02 14:21:33 -0500

    Pool psql

--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -1,1 +1,14 @@
 import pool from "../db";
+import { type Conversation } from "../types/domain";
+
+export async function insertConversation(): Promise<Conversation> {
+    const query = `
+        INSERT INTO conversations (title)
+        VALUES ($1)
+        RETURNING id, title, created_at, updated_at;
+    `;
+    const values = ['New Conversation'];
+
+    const result = await pool.query<Conversation>(query, values);
+    return result.rows[0]!;
+}
